6a2baf09b9d88a98fc4d9adc8fc4e872e9977ea8
Motiullah Sajit <sajitmotiullah@gmail.com>
2024-07-30 00:28:47 +0600

    cart added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = () => {
   const user = null;
@@ -60,7 +61,9 @@ const Navbar = () => {
                     </div>
                   )}
 
-                  <div className="ml-4 flow-root lg:ml-6">Cart</div>
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
                 </div>
               </div>
             </div>
