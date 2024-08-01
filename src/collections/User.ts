0e4225f9153a65dff9d35e0b637d063d2f40f605
Motiullah Sajit <sajitmotiullah@gmail.com>
2024-08-01 21:48:54 +0600

    auth and menu complete

--- src/collections/User.ts
+++ src/collections/User.ts
@@ -2,7 +2,13 @@ import { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
   slug: "users",
-  auth: true,
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `<p>Welcome to DigitalHippo, Please verify you email. <a href='${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}'>Verify Email</a></p>`;
+      },
+    },
+  },
   access: {
     read: () => true,
     create: () => true,
--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -2,6 +2,7 @@ import { publicProcedure, router } from "./trpc";
 import { getPayloadClient } from "../get-payload";
 import { AuthCredentialsValidator } from "../lib/validators/account-credential-validator";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
 export const authRouter = router({
   createPayloadUser: publicProcedure
@@ -31,5 +32,43 @@ export const authRouter = router({
       });
 
       return { success: true, sentToEmail: email };
+    }),
+
+  verifyEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .query(async ({ input }) => {
+      const { token } = input;
+      const payload = await getPayloadClient();
+      const isVerified = await payload.verifyEmail({
+        collection: "users",
+        token,
+      });
+      if (!isVerified) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+      return { success: true };
+    }),
+
+  signIn: publicProcedure
+    .input(AuthCredentialsValidator)
+    .mutation(async ({ input, ctx }) => {
+      const { email, password } = input;
+      const { res } = ctx;
+
+      const payload = await getPayloadClient();
+
+      try {
+        await payload.login({
+          collection: "users",
+          data: {
+            email,
+            password,
+          },
+          res,
+        });
+
+        return { success: true };
+      } catch (err) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+      }
     }),
 });
