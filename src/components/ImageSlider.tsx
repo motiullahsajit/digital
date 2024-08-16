1d36b3018d9b1395daf6fa8ea4d61d0f40967eea
Motiullah Sajit <sajitmotiullah@gmail.com>
2024-08-16 22:35:04 +0600

    deployment update

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -33,6 +33,8 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
     });
   }, [swiper, urls]);
 
+  console.log("urls", urls);
+
   const activeStyles =
     "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
   const inactiveStyles = "hidden text-gray-400";
